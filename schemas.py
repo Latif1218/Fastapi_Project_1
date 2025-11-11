bfd764686665e61fdad26ae01476d93946678298
Latif <mdsabdullotif@gmail.com>
2025-11-11 11:23:27 +0600

    add and update

--- schemas.py
+++ schemas.py
@@ -25,4 +25,10 @@ class SignUpModel(BaseModel):
         }
 
 
-    
+class Setting(BaseModel):
+    authjwt_secret_key:str='02b283c3e65f6a8aada2d5853d963c516b36fd94997064d74c362fec8860b0f3'
+
+
+class LoginModel(BaseModel):
+    username:str
+    password:str
